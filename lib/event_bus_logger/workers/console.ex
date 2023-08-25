4b4558fd404866726f462241a15c8750c85e8982
fabianskier <fabianskier@icloud.com>
2023-08-25 17:58:15 -0400

    Filter EventBus.Model.Event

--- lib/event_bus_logger/workers/console.ex
+++ lib/event_bus_logger/workers/console.ex
@@ -30,9 +30,17 @@ defmodule EventBus.Logger.Worker.Console do
   @doc false
   def handle_cast({topic, id}, state) do
     event = EventBus.fetch_event({topic, id})
-    log(event, light_logging: Config.light_logging?())
+    filtered_event = filter_event_fields(event, [:data])  # Excludes :data
+    log(filtered_event, light_logging: Config.light_logging?())
     EventBus.mark_as_completed({EventBus.Logger, topic, id})
     {:noreply, state}
+  end
+
+  defp filter_event_fields(event, exclude_keys) when is_list(exclude_keys) do
+      event
+      |> Map.from_struct()
+      |> Enum.reject(fn {k, _} -> k in exclude_keys end)
+      |> Enum.into(%{})
   end
 
   defp log(event, light_logging: true) do
